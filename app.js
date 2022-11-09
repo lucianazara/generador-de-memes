ba6395e012bda376e9de2f27d6c16b56a8eb87af
Lu Zara <lucininiz@gmail.com>
2022-11-08 22:07:12 -0300

    Se agregan la funcionalidad de hacer click en texto e imagen y que se abra el panel

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
 let $ = (selector) => document.querySelector(selector);
 let $$ = (selector) => document.querySelector(selector);
+
+let enlaceImagen = $("#imagen-nav");
+let panelImagen = $(".seccion-imagen");
+let enlaceTexto = $("#texto-nav");
+let panelTexto = $(".seccion-texto");
+
+enlaceImagen.addEventListener("click", function() {
+    panelImagen.classList.add("desplegar");
+    panelTexto.classList.remove("desplegar");
+})
+
+enlaceTexto.addEventListener("click", function() {
+    panelTexto.classList.add("desplegar");
+    panelImagen.classList.remove("desplegar");
+})
