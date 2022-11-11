435c6ee9a2fd7c9943a561f73a896679f840ec97
Lu Zara <lucininiz@gmail.com>
2022-11-11 13:12:20 -0300

    Se implementa el modo oscuro y modo claro

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ let enlaceImagen = $("#imagen-nav");
 let panelImagen = $(".seccion-imagen");
 let enlaceTexto = $("#texto-nav");
 let panelTexto = $(".seccion-texto");
+let botonModoOscuro =$("#modo-oscuro");
+let body = $("body");
 
 enlaceImagen.addEventListener("click", function() {
     panelImagen.classList.add("desplegar");
@@ -15,3 +17,20 @@ enlaceTexto.addEventListener("click", function() {
     panelTexto.classList.add("desplegar");
     panelImagen.classList.remove("desplegar");
 })
+
+window.addEventListener("load", function() {
+    document.body.classList.add("modo-oscuro");
+})
+
+botonModoOscuro.addEventListener("click", function(){
+    document.body.classList.add("modo-claro");
+    if(botonModoOscuro.innerHTML == "Modo oscuro"){
+        document.body.classList.add("modo-claro");
+        botonModoOscuro.innerHTML = "Modo claro";
+    } else {
+        document.body.classList.remove("modo-claro");
+        botonModoOscuro.innerHTML = "Modo oscuro";
+      
+    }
+})
+
