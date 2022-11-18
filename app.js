f9e21a933f712ce3c5c2acaf7a96d750e3fa0fab
Lu Zara <lucininiz@gmail.com>
2022-11-17 23:02:16 -0300

    Se agrega la funcionalidad de abrir y cerrar los paneles de Imagen y Texto

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ let enlaceTexto = $("#texto-nav");
 let panelTexto = $(".seccion-texto");
 let botonModoOscuro =$("#modo-oscuro");
 let body = $("body");
+let botonCerrar = $(".boton-cerrar");
+let botonCerrarTexto = $("#boton-cerrar-texto");
+let botonCerrarImagen = $("#boton-cerrar-imagen");
+let menu = $(".menu");
 
 enlaceImagen.addEventListener("click", function() {
     panelImagen.classList.add("desplegar");
@@ -30,7 +34,28 @@ botonModoOscuro.addEventListener("click", function(){
     } else {
         document.body.classList.remove("modo-claro");
         botonModoOscuro.innerHTML = "Modo oscuro";
-      
     }
 })
 
+
+// panelImagen.addEventListener("onload", function(){
+//     panelImagen.classList.add("ocultar");
+// })
+panelImagen.addEventListener("click", function(){
+    panelImagen.classList.add("desplegar");
+    panelImagen.classList.remove("desplegar");
+    })
+
+botonCerrarImagen.addEventListener("click", function(){
+    panelImagen.classList.remove("desplegar");
+})
+
+
+panelTexto.addEventListener("click", function(){
+    panelTexto.classList.add("desplegar");
+    panelTexto.classList.remove("desplegar");
+    })
+
+botonCerrarTexto.addEventListener("click", function(){
+    panelTexto.classList.remove("desplegar");
+})
