3d6cbb1c0236cff0b4754f6ba8c325a4acc0338c
Lu Zara <lucininiz@gmail.com>
2023-03-26 22:41:24 -0300

    Se modifican algunos detalles

--- js/app.js
+++ js/app.js
@@ -1,64 +1,65 @@
-const $ = selector => document.querySelector(selector);
-const $$ = selector => document.querySelectorAll(selector);
+const $ = selector => document.querySelector(selector)
+const $$ = selector => document.querySelectorAll(selector)
 
-let enlaceImagen = $("#imagen-nav");
-let panelImagen = $(".seccion-imagen");
-let botonAbrirImagen = $("#btn-abrir-imagen")
-let botonAbrirTexto= $("#btn-abrir-texto")
-let seccionImagen = $("#seccion-panel-imagen");
-let enlaceTexto = $("#texto-nav");
-let panelTexto = $(".seccion-texto");
-let seccionTexto = $("#seccion-panel-texto");
-let botonModoOscuro =$("#modo-oscuro");
-let body = $("body");
-let botonCerrar = $(".boton-cerrar");
-let botonCerrarTexto = $("#boton-cerrar-texto");
-let botonCerrarImagen = $("#boton-cerrar-imagen");
-let menu = $(".menu");
-let imagenURL = $("#url");
-let memeContenedor = $("#meme-contenedor");
-let memeGenerador = $("#meme-generador")
-let memeImagen = $(".meme-imagen");
-let textoSuperior = $("#meme-texto-superior");
-let textoInferior = $("#meme-texto-inferior");
-let parrafoTextoSuperior = $("#parrafo-texto-superior");
-let parrafoTextoInferior = $("#parrafo-texto-inferior");
-let inputTextoSuperior = $("#texto-superior");
-let inputTextoInferior = $("#texto-inferior");
-let sinTextoSuperior = $("#sin-texto-superior");
-let sinTextoInferior = $("#sin-texto-inferior");
-let contenedorTextoSuperior = $("#contenedor-texto-superior");
-let contenedorTextoInferior = $("#contenedor-texto-inferior");
-let tipografia = $("#tipografia");
-let tamanoFuente = $("#tamano-fuente");
-let botonAlineacion = $(".botones-alineacion");
-let botonIzquierda = $("#btn-izquierda");
-let botonCentro = $("#btn-centro");
-let botonDerecha = $("#btn-derecha");
-let colorTexto = $("#color-texto");
-let colorFondo = $("#color-fondo");
-let fondoTransparente = $("#fondo-transparente")
-let textosMeme = $$(".meme-generador-texto");
-let botonContornoClaro = $("#btn-contorno-claro");
-let botonContornoOscuro = $("#btn-contorno-oscuro");
-let espaciado = $("#espaciado");
-let interlineado = $("#interlineado");
+const enlaceImagen = $("#imagen-nav")
+const panelImagen = $(".seccion-imagen")
+const botonAbrirImagen = $("#btn-abrir-imagen")
+const botonAbrirTexto= $("#btn-abrir-texto")
+const seccionImagen = $("#seccion-panel-imagen")
+const enlaceTexto = $("#texto-nav")
+const panelTexto = $(".seccion-texto")
+const seccionTexto = $("#seccion-panel-texto")
+const botonModoOscuro =$("#modo-oscuro")
+const body = $("body")
+const botonCerrar = $(".boton-cerrar")
+const botonCerrarTexto = $("#boton-cerrar-texto")
+const botonCerrarImagen = $("#boton-cerrar-imagen")
+const menu = $(".menu")
+const imagenURL = $("#url")
+const memeContenedor = $("#meme-contenedor")
+const memeGenerador = $("#meme-generador")
+const memeImagen = $(".meme-imagen")
+const texto = $(".texto")
+const textoSuperior = $("#meme-texto-superior")
+const textoInferior = $("#meme-texto-inferior")
+const parrafoTextoSuperior = $("#parrafo-texto-superior")
+const parrafoTextoInferior = $("#parrafo-texto-inferior")
+const inputTextoSuperior = $("#texto-superior")
+const inputTextoInferior = $("#texto-inferior")
+const sinTextoSuperior = $("#sin-texto-superior")
+const sinTextoInferior = $("#sin-texto-inferior")
+const contenedorTextoSuperior = $("#contenedor-texto-superior")
+const contenedorTextoInferior = $("#contenedor-texto-inferior")
+const tipografia = $("#tipografia")
+const tamanoFuente = $("#tamano-fuente")
+const botonAlineacion = $(".botones-alineacion")
+const botonIzquierda = $("#btn-izquierda")
+const botonCentro = $("#btn-centro")
+const botonDerecha = $("#btn-derecha")
+const colorTexto = $("#color-texto")
+const colorFondo = $("#color-fondo")
+const fondoTransparente = $("#fondo-transparente")
+const textosMeme = $$(".meme-generador-texto")
+const botonContornoClaro = $("#btn-contorno-claro")
+const botonContornoOscuro = $("#btn-contorno-oscuro")
+const espaciado = $("#espaciado")
+const interlineado = $("#interlineado")
 
 //VARIABLES SECCION IMAGEN
-let colorFondoMeme = $("#color-fondo-meme");
-let efectoEstablecido = $("#efecto-preestablecido");
-let contenedorMeme = $(".contenedor-meme-imagen");
-let brillo = $("#brightness");
-let opacidad = $("#opacity");
-let contraste = $("#contrast");
-let desenfoque = $("#blur");
-let escalaGrises = $("#grayscale");
-let sepia = $("#sepia");
-let hue = $("#hue");
-let saturacion = $("#saturate");
-let negativo = $("#invert");
-let botonReestablecer = $("#boton-reestablecer");
-let botonDescargar = $("#boton-descargar");
+const colorFondoMeme = $("#color-fondo-meme")
+const efectoEstablecido = $("#efecto-preestablecido")
+const contenedorMeme = $(".contenedor-meme-imagen")
+const brillo = $("#brightness")
+const opacidad = $("#opacity")
+const contraste = $("#contrast")
+const desenfoque = $("#blur")
+const escalaGrises = $("#grayscale")
+const sepia = $("#sepia")
+const hue = $("#hue")
+const saturacion = $("#saturate")
+const negativo = $("#invert")
+const botonReestablecer = $("#boton-reestablecer")
+const botonDescargar = $("#boton-descargar")
 
 botonAbrirImagen.addEventListener("click", () => {
     if(seccionImagen.classList.contains("oculto") || seccionTexto.style.display === "block" ){
@@ -79,17 +80,17 @@ botonAbrirTexto.addEventListener("click", () => {
 })
 
 window.addEventListener("load", () => {
-    document.body.classList.add("modo-oscuro");
+    document.body.classList.add("modo-oscuro")
 })
 
 botonModoOscuro.addEventListener("click", () => {
-    document.body.classList.add("modo-claro");
+    document.body.classList.add("modo-claro")
     if(botonModoOscuro.innerHTML == "Modo oscuro"){
-        document.body.classList.add("modo-claro");
-        botonModoOscuro.innerHTML = "Modo claro";
+        document.body.classList.add("modo-claro")
+        botonModoOscuro.innerHTML = "Modo claro"
     } else {
-        document.body.classList.remove("modo-claro");
-        botonModoOscuro.innerHTML = "Modo oscuro";
+        document.body.classList.remove("modo-claro")
+        botonModoOscuro.innerHTML = "Modo oscuro"
     }
 })
 
@@ -106,101 +107,102 @@ botonCerrarTexto.addEventListener("click", () =>{
 // AGREGAR IMAGEN
 
 imagenURL.addEventListener("input", event => {
-  let url = event.target.value;
-  memeImagen.style.backgroundImage = `url(${url})`;
+  const url = event.target.value
+  memeImagen.style.backgroundImage = `url(${url})`
 })
 
 // MODIFICAR TEXTOS
 inputTextoSuperior.addEventListener("input", event => {
-    parrafoTextoSuperior.innerText = event.target.value;
+    parrafoTextoSuperior.innerText = event.target.value
 })
 
 inputTextoInferior.addEventListener("input", event => {
-    parrafoTextoInferior.innerText = event.target.value;
+    parrafoTextoInferior.innerText = event.target.value
 })
 
 // OCULTAR TEXTOS
 sinTextoSuperior.addEventListener("click", () => {
-    contenedorTextoSuperior.classList.toggle("oculto");
+    contenedorTextoSuperior.classList.toggle("oculto")
 })
 
 sinTextoInferior.addEventListener("click", () => {
-    contenedorTextoInferior.classList.toggle("oculto");
+    contenedorTextoInferior.classList.toggle("oculto")
 })
 
 // CAMBIAR TIPOGRAFIA
 
 tipografia.addEventListener("change", event => {
-    let tipografiaElegida = event.target.value;
-    parrafoTextoSuperior.style.fontFamily = tipografiaElegida;
-    parrafoTextoInferior.style.fontFamily = tipografiaElegida;
+    const tipografiaElegida = event.target.value
+    parrafoTextoSuperior.style.fontFamily = tipografiaElegida
+    parrafoTextoInferior.style.fontFamily = tipografiaElegida
 })
 
 tipografia.addEventListener("change", event =>{
-    let tipografiaElegida = event.target.value;
-    parrafoTextoSuperior.style.fontFamily = tipografiaElegida;
-    parrafoTextoInferior.style.fontFamily = tipografiaElegida;
+    const tipografiaElegida = event.target.value
+    parrafoTextoSuperior.style.fontFamily = tipografiaElegida
+    parrafoTextoInferior.style.fontFamily = tipografiaElegida
 })
 
 tamanoFuente.addEventListener("input", event =>{
-    let tamanoFuenteElegido = event.target.value;
-    parrafoTextoSuperior.style.fontSize = `${tamanoFuenteElegido}px`;
-    parrafoTextoInferior.style.fontSize = `${tamanoFuenteElegido}px`;
+    const tamanoFuenteElegido = event.target.value
+    parrafoTextoSuperior.style.fontSize = `${tamanoFuenteElegido}px`
+    parrafoTextoInferior.style.fontSize = `${tamanoFuenteElegido}px`
 })
 
 // ALINEACION DE TEXTO
 
-botonIzquierda.addEventListener("click", () =>{
-    parrafoTextoSuperior.style.textAlign = "left";
+botonIzquierda.addEventListener("click", event =>{
+    const alineacionElegida = event.target.value
+    parrafoTextoSuperior.style.textAlign = `${alineacionElegida}`
 })
 
 botonCentro.addEventListener("click", () =>{
-    parrafoTextoSuperior.style.textAlign = "center";
+    parrafoTextoSuperior.style.textAlign = "center"
 })
 
 botonDerecha.addEventListener("click", () =>{
-    parrafoTextoSuperior.style.textAlign = "right";
+    parrafoTextoSuperior.style.textAlign = "right"
 })
 
 //CAMBIAR COLOR
 
 colorTexto.addEventListener("input", event => {
-    let colorElegidoTexto = event.target.value;
-    parrafoTextoSuperior.style.color = colorElegidoTexto;
-    parrafoTextoInferior.style.color = colorElegidoTexto;
+    const colorElegidoTexto = event.target.value
+    parrafoTextoSuperior.style.color = colorElegidoTexto
+    parrafoTextoInferior.style.color = colorElegidoTexto
 })
 
 colorFondo.addEventListener("change", event => {
-    let colorElegidoFondo = event.target.value;
-    contenedorTextoSuperior.style.backgroundColor = colorElegidoFondo;
-    contenedorTextoInferior.style.backgroundColor= colorElegidoFondo;
+    const colorElegidoFondo = event.target.value
+    contenedorTextoSuperior.style.backgroundColor = colorElegidoFondo
+    contenedorTextoInferior.style.backgroundColor= colorElegidoFondo
 })
 
 // FONDO TRANSPARENTE
 fondoTransparente.addEventListener("click", () => {
-       parrafoTextoSuperior.classList.add("fondo-transparente");
-        contenedorTextoSuperior.classList.add("ocultar");
-        parrafoTextoSuperior.classList.add("fondo-transparente");
-        contenedorTextoInferior.classList.add("ocultar");
+       parrafoTextoSuperior.classList.add("fondo-transparente")
+        contenedorTextoSuperior.classList.add("ocultar")
+        parrafoTextoSuperior.classList.add("fondo-transparente")
+        contenedorTextoInferior.classList.add("ocultar")
 
 })
 
 // CONTORNO 
 botonContornoClaro.addEventListener("click", () => {
-    parrafoTextoSuperior.classList.toggle("contorno-claro");
-    parrafoTextoInferior.classList.toggle("contorno-claro");
+    parrafoTextoSuperior.classList.toggle("contorno-claro")
+    parrafoTextoInferior.classList.toggle("contorno-claro")
 
 })
 
 botonContornoOscuro.addEventListener("click", () => {
-    parrafoTextoSuperior.classList.toggle("contorno-oscuro");
+    parrafoTextoSuperior.classList.toggle("contorno-oscuro")
     parrafoTextoInferior.classList.toggle("contorno-oscuro")
 
 })
 
 //ESPACIADO
 espaciado.addEventListener("input", event => {
-    let espaciadoElegido = event.target.value
+    const espaciadoElegido = event.target.value
     contenedorTextoSuperior.style.paddingTop = `${espaciadoElegido}px`
     contenedorTextoSuperior.style.paddingBottom = `${espaciadoElegido}px`
     contenedorTextoInferior.style.paddingTop = `${espaciadoElegido}px`
@@ -217,7 +219,7 @@ interlineado.addEventListener("click", event => {
 //SECCION IMAGEN
 
 colorFondoMeme.addEventListener("input", event => {
-    let colorFondoMemeElegido = event.target.value
+    const colorFondoMemeElegido = event.target.value
     memeImagen.style.backgroundColor = colorFondoMemeElegido
 })
 
@@ -225,7 +227,7 @@ colorFondoMeme.addEventListener("input", event => {
 //EFECTO DE FONDO
 
 efectoEstablecido.addEventListener("change", event => {
- let efecto = event.target.value
+ const efecto = event.target.value
     if(efecto == "Ninguno"){
         contenedorMeme.style.backgroundBlendMode = "normal"
     }else if(efecto == "Aclarar"){
@@ -244,46 +246,46 @@ efectoEstablecido.addEventListener("change", event => {
 
 //FILTROS
 brillo.addEventListener("change", event => {
-    let brilloElegido = event.target.value
+    const brilloElegido = event.target.value
     memeImagen.style.filter = `brightness(${brilloElegido}`
 })
 
 opacidad.addEventListener("change", event => {
-    let opacidadElegida = event.target.value
+    const opacidadElegida = event.target.value
     memeImagen.style.filter = `opacity(${opacidadElegida}`
 })
 
 contraste.addEventListener("change", event => {
-    let contrasteElegido = event.target.value
+    const contrasteElegido = event.target.value
     memeImagen.style.filter = `contrast(${contrasteElegido}`
 })
 
 desenfoque.addEventListener("change", event => {
-    let desenfoqueElegido = event.target.value
+    const desenfoqueElegido = event.target.value
     memeImagen.style.filter = `blur(${desenfoqueElegido}px`
 })
 
 escalaGrises.addEventListener("change", event => {
-    let escalaGrisesElegida = event.target.value
+    const escalaGrisesElegida = event.target.value
     memeImagen.style.filter = `grayscale(${escalaGrisesElegida}`
 })
 
 sepia.addEventListener("change", event => {
-    let sepiaElegido = event.target.value
+    const sepiaElegido = event.target.value
     memeImagen.style.filter = `sepia(${sepiaElegido}`
 })
 hue.addEventListener("change", event => {
-    let hueElegido = event.target.value
+    const hueElegido = event.target.value
     memeImagen.style.filter = `hue-rotate(${hueElegido}deg`
 })
 
 saturacion.addEventListener("change", event => {
-    let saturacionElegida = event.target.value
+    const saturacionElegida = event.target.value
     memeImagen.style.filter = `saturate(${saturacionElegida}`
 })
 
 negativo.addEventListener("change", event => {
-    let negativoElegido = event.target.value
+    const negativoElegido = event.target.value
     memeImagen.style.filter = `invert(${negativoElegido}`
 })
 
